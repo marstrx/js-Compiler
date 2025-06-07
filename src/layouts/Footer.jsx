52107a2401e42bb9fc8743d5a7f62e2f3a29084c
Marouane Bachar <marstrone@gmail.com>
2025-06-07 17:36:46 +0200

    footer added

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './Footer.css'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+        <div>
+            <p className="footer-text">
+            &copy; {new Date().getFullYear()} JsCompiler. All rights reserved.
+            </p>
+        </div>
+        <div className="social-links">
+            <p>Follow me on </p>
+            <p><i class="bi bi-github"></i>
+            <a href="https://github.com"> GitHub</a></p>
+            <p><i class="bi bi-twitter-x"></i>
+            <a href="https://twitter.com"> Elon</a></p>
+            <p><i class="bi bi-linkedin"></i>
+            <a href="https://linkedin.com"> LinkedIn</a></p>
+        </div>
+    </footer>
   )
 }
 
