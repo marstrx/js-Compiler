565bfee5bc71ed81b1a4d00548b33f06a692891a
Marouane Bachar <marstrone@gmail.com>
2025-06-20 23:39:16 +0200

    docs css

--- src/pages/Docs.jsx
+++ src/pages/Docs.jsx
@@ -1,34 +1,34 @@
 import React from 'react'
-
+import "./Docs.css";
 function Docs() {
   return (
-    <>
-    <h1>Documentation</h1>
+    <div className="docs-container">
+    <h3>Documentation</h3>
     <p>Welcome to the documentation page. Here you will find all the information you need to get started with our project.</p>
-    <h2>Getting Started</h2>
+    <h4>Getting Started</h4>
     <p>To get started, you can follow the steps below:</p>
-    <ol>
+    <ol className="docs-get-started">
       <li>Clone the repository from GitHub.</li>
       <li>Install the necessary dependencies using npm or yarn install</li>
       <li>Run the development server to see the project in action.</li>
       <li>Explore the codebase and start making changes!</li>
     </ol>
-    <h2>Features</h2>
+    <h4 className='docs-features'>Features</h4>
     <p>This project includes several features that make it easy to use:</p>
-    <ul>
+    <ul className='docs-features-list'>
       <li>Responsive design that works on all devices.</li>
       <li>Modular architecture for easy maintenance and scalability.</li>
     </ul>
-    <h2>Contributing</h2>
+    <h4 className='docs-contributing'>Contributing</h4>
     <p>If you would like to contribute to this project, please follow these steps:</p>
-    <ol>
+    <ol className='docs-contributing-list'>
       <li>Fork the repository on GitHub.</li>
       <li>Create a new branch for your feature or bug fix.</li>
       <li>Make your changes and commit them with a clear message.</li>
       <li>Push your changes to your forked repository.</li>
       <li>Submit a pull request to the main repository.</li>
     </ol>
-    </>
+    </div>
       
   )
 }
