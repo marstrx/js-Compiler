66cef4347c653debdd0681ab23df67212bc4a294
Marouane Bachar <marstrone@gmail.com>
2025-06-20 23:08:29 +0200

    docs added

--- src/pages/Docs.jsx
+++ src/pages/Docs.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 function Docs() {
   return (
-    <div>Docs</div>
+    <>
+    <h1>Documentation</h1>
+    <p>Welcome to the documentation page. Here you will find all the information you need to get started with our project.</p>
+    <h2>Getting Started</h2>
+    <p>To get started, you can follow the steps below:</p>
+    <ol>
+      <li>Clone the repository from GitHub.</li>
+      <li>Install the necessary dependencies using npm or yarn install</li>
+      <li>Run the development server to see the project in action.</li>
+      <li>Explore the codebase and start making changes!</li>
+    </ol>
+    <h2>Features</h2>
+    <p>This project includes several features that make it easy to use:</p>
+    <ul>
+      <li>Responsive design that works on all devices.</li>
+      <li>Modular architecture for easy maintenance and scalability.</li>
+    </ul>
+    <h2>Contributing</h2>
+    <p>If you would like to contribute to this project, please follow these steps:</p>
+    <ol>
+      <li>Fork the repository on GitHub.</li>
+      <li>Create a new branch for your feature or bug fix.</li>
+      <li>Make your changes and commit them with a clear message.</li>
+      <li>Push your changes to your forked repository.</li>
+      <li>Submit a pull request to the main repository.</li>
+    </ol>
+    </>
+      
   )
 }
 
