78006f330a9719155d663e7a0a30fdba192810d4
Marouane Bachar <marstrone@gmail.com>
2025-08-02 19:47:31 +0200

    some css added to the nav bar

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,33 +1,76 @@
-import jslogo from "/src/assets/JavaScript-logo.png";
-import { Link } from "react-router-dom";
 import { useState } from "react";
-import "./Header.css";
+import { Link } from "react-router-dom";
+import jslogo from "/src/assets/JavaScript-logo.png";
+import "./Header.css"
 
 function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(prev => !prev);
-  };
+  // false until the user click
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="header-container">
-      <div className="header-logo">
-        <img className="jslogo" src={jslogo} alt="javascript" />
-        <h2>JsCompiler</h2>
+    <header className="bg-zinc-400 text-white fixed w-full top-0 left-0 shadow-md z-50">
+      <div className="flex justify-between items-center px-4 py-3">
+        <div className="flex items-center gap-1 ">
+          <img src={jslogo} alt="JS Logo" className="w-10 h-10 rounded-full mb-2.5" />
+          <h4 className="font-mono font-bold">JsCompiler</h4>
+        </div>
+
+        <ul className="hidden md:flex items-center gap-6 text-md font-medium">
+          <li>
+            <Link to="/" className="no-underline text-white" >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/docs" className=" text-white">
+              Docs
+            </Link>
+          </li>
+          <li>
+            <Link to="/examples" className=" text-white">
+              Examples
+            </Link>
+          </li>
+        </ul>
+
+        <button className="md:hidden focus:outline-none" onClick={() => setIsOpen(!isOpen)}>
+
+          <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+
+            {isOpen ? 
+
+            (<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />) 
+              : 
+            (<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16"/>)
+            }
+
+          </svg>
+        </button>
       </div>
-      <ul className={menuOpen ? "show-menu" : ""}>
-        <li><Link to={"/"}>Home</Link></li>
-        <li><Link to={"/docs"}>Docs</Link></li>
-        <li><Link to={"/examples"}>Examples</Link></li>
-      </ul>
-      <i
-        onClick={toggleMenu}
-        id="menuIcon"
-        className={`bi ${menuOpen ? "bi-x-lg" : "bi-list"} fs-3 menuIcon`}
-      ></i>
+
+      {isOpen && (
+        <ul className="md:hidden flex flex-col items-center px-6 pb-4 gap-3 text-md font-medium">
+          <li>
+            <Link to="/" onClick={() => setIsOpen(false)} className="no-underline text-white">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/docs" onClick={() => setIsOpen(false)} className="no-underline text-white">
+              Docs
+            </Link>
+          </li>
+          <li>
+            <Link to="/examples" onClick={() => setIsOpen(false)} className="no-underline text-white">
+              Examples
+            </Link>
+          </li>
+        </ul>
+      )}
     </header>
   );
 }
 
 export default Header;
+
+
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,38 @@
 import React from 'react';
 import homepage from "../../assets/Images/homepage.jpg";
-import './Home.css';
 import { Link } from 'react-router-dom';
 import Content from '../Content';
 
 export default function Home() {
   return (
     <>
-    <div className="home">
-        <div className='homeLeftSide'>
-            <img src={homepage} alt="" />
+    <div className="flex flex-row gap-4 my-40 justify-center items-center mr-5.5 ml-5.5" >
+        <div className='shadow-2xl'>
+            <img className='rounded-2xl' src={homepage} alt="" />
         </div>
-        <div className='homeRightSide'>
-            <h1>Welcome to JsCompiler</h1>
+        <div className=''>
+            <h1 className='font-mono font-bold text-shadow-amber-100'>Welcome to JsCompiler</h1>
             <p>
                 JsCompiler is a powerful online JavaScript compiler that allows you to write, run, and share JavaScript code directly in your browser.
             </p>
-            <div className='homeBtns'>
-                <Link to="/" className='btn btn-primary'><i class="bi bi-lightning-charge"></i>Try Now</Link>
-                <Link to="/examples" className='btn btn-secondary'><i class="bi bi-book"></i> View Examples</Link>
+            <div className='flex flex-row gap-7 ml-10'>
+                
+                <button className='flex items-center gap-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-2 shadow-md transition-all duration-300'>
+                    <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
+                    </svg>
+                    Try now
+                </button>
+                <button className='flex items-center gap-2  bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-2 shadow-md transition-all duration-300'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+                    </svg>
+                    Examples
+                </button>
+
+
+                {/* <Link to="/" className='btn btn-primary'><i class="bi bi-lightning-charge"></i>Try Now</Link> */}
+                {/* <Link to="/examples" className='btn btn-secondary'><i class="bi bi-book"></i> View Examples</Link> */}
             </div>
         </div>
         
@@ -27,3 +41,7 @@ export default function Home() {
     </>
   )
 }
+
+
+
+
