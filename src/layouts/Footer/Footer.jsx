da674905d310b3c1bcf4c0f55d5f5e1f569aa1bf
Marouane Bachar <marstrone@gmail.com>
2025-08-05 23:00:16 +0200

    i fix something on the footer section

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -4,23 +4,23 @@ function Footer() {
   return (
     
     <>
-    <footer className='bg-zinc-500 text-white gap-3 block md:flex justify-between p-3 font-mono font-bold'>
+    <footer className='bg-zinc-500 text-white gap-3 block md:flex justify-between p-3 font-mono'>
       <div>
         <p className="flex items-center justify-center ml-5.5">
           &copy; {new Date().getFullYear()} JsCompiler. All rights reserved.
         </p>
       </div>
-      <div className='flex gap-5 '>
+      <div className='flex gap-4 justify-center '>
         <div>
           <p>Follow me on </p>
         </div>
 
-        <div className='flex gap-3.5  mr-5.5'>
-          <div ><a style={{textDecoration:"none"}} className='text-white' href="https://github.com/marstrx"> GitHub</a><i class="bi bi-github"></i>
+        <div className='flex gap-6  mr-5.5'>
+          <div ><a style={{textDecoration:"none"}} className='text-white' href="https://github.com/marstrx"></a><i class="bi bi-github"></i>
           </div>
-          <div><a style={{textDecoration:"none"}} className='text-white' href="https://x.com/marstr__1"> Elon</a><i class="bi bi-twitter-x"></i>
+          <div><a style={{textDecoration:"none"}} className='text-white' href="https://x.com/marstr__1"></a><i class="bi bi-twitter-x"></i>
           </div>
-          <div><a style={{textDecoration:"none"}} className='text-white'  href="https://www.linkedin.com/in/marouanebachar"> LinkedIn</a><i class="bi bi-linkedin"></i>
+          <div><a style={{textDecoration:"none"}} className='text-white'  href="https://www.linkedin.com/in/marouanebachar"></a><i class="bi bi-linkedin"></i>
           </div>
         </div>
 
