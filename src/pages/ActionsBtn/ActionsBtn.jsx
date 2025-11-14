255e91f994bc58b79baf6d2f331592511b728e3b
Marouane Bachar <marstrone@gmail.com>
2025-11-14 18:07:40 +0100

    react-hot-toast

--- src/pages/ActionsBtn/ActionsBtn.jsx
+++ src/pages/ActionsBtn/ActionsBtn.jsx
@@ -1,11 +1,28 @@
-import React, { useContext } from 'react'
-import { EditorContext } from '../Context/EditorContext';
+import React, { useContext } from "react";
+import { EditorContext } from "../Context/EditorContext";
+import toast,{Toaster} from "react-hot-toast";
 
 function ActionsBtn() {
-  const {htmlCode,cssCode,jsCode} = useContext(EditorContext);
-  const {setHtmlCode,setCssCode,setJsCode} = useContext(EditorContext);
-  const { setOutputSrcDoc} = useContext(EditorContext);
-  const allCodesFile=`
+  // toasters functions
+  const HandleClearCode =()=>{
+    toast.success("Deleted Succussfully!");
+  };
+  const HandleSaveCode =()=>{
+    toast.success("Code Saved Succussfully!");
+  };
+  const HandleRunCode =()=>{
+    toast.success("Code Runed Succussfully!");
+  };
+  const {
+    htmlCode,
+    cssCode,
+    jsCode,
+    setHtmlCode,
+    setCssCode,
+    setJsCode,
+    setOutputSrcDoc,
+  } = useContext(EditorContext);
+  const allCodesFile = `
     <html>
     <head>
     <style>${cssCode}</style>
@@ -15,52 +32,101 @@ function ActionsBtn() {
     <script>${jsCode}</script>
     </body>
     </html>
-    `
-  ;
-  // run code 
-  const handleRunCode =()=>{
+    `;
+  // run code
+  const handleRunCode = () => {
     setOutputSrcDoc(allCodesFile);
-  }
-  // clear code 
-  const handleClear =()=>{
+    HandleRunCode();
+  };
+  // clear code
+  const handleClear = () => {
     setHtmlCode("");
     setCssCode("");
     setJsCode("");
     setOutputSrcDoc("");
-  }
-  // save code 
-  const handleSaveCode =()=>{
-        const blob = new Blob([allCodesFile],{
-            type: "text/html"
-        });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.href = url;
-        a.download ="marstrx.html";
-        a.click();
-        URL.revokeObjectURL(url);
-  }
+    HandleClearCode()
+  };
+  // save code
+  const handleSaveCode = () => {
+    const blob = new Blob([allCodesFile], {
+      type: "text/html",
+    });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "marstrx.html";
+    a.click();
+    URL.revokeObjectURL(url);
+    HandleSaveCode();
+  };
   return (
     <>
-    <div className='flex gap-2 font-mono'>
-        <button onClick={handleRunCode} className='flex-1 bg-yellow-600 text-white py-2 rounded font-mono flex items-center justify-center'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+      <div className="flex gap-2 font-mono">
+        <button
+          onClick={handleRunCode}
+          className="flex-1 bg-yellow-600 text-white py-2 rounded font-mono flex items-center justify-center"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+            />
           </svg>
- Run</button>
-        <button onClick={handleSaveCode} className='flex-1 bg-yellow-600 text-white py-2 rounded font-mono flex items-center justify-center'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+          Run
+        </button>
+        <button
+          onClick={handleSaveCode}
+          className="flex-1 bg-yellow-600 text-white py-2 rounded font-mono flex items-center justify-center"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+            />
           </svg>
-           Save</button>
-        <button className='flex-1 bg-yellow-600 text-white py-2 rounded font-mono flex items-center justify-center' onClick={handleClear}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+          Save
+        </button>
+        <button
+          className="flex-1 bg-yellow-600 text-white py-2 rounded font-mono flex items-center justify-center"
+          onClick={handleClear}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+            />
           </svg>
-           Clear</button>
-    </div>
+          
+          Clear
+        </button>
+      </div>
+      <Toaster />
     </>
-  )
+  );
 }
 
 export default ActionsBtn;
