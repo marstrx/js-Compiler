a5546c1c42174f1c287dceab3be1e35ec752b5e3
marstrx <marstrone@gmail.com>
2025-12-15 21:25:52 +0100

    header fixed

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import jslogo from "/src/assets/JavaScript-logo.png";
-import "./Header.css"
+import "./Header.css";
 
 function Header() {
   // false until the user click
@@ -10,45 +10,83 @@ function Header() {
   return (
     <header className="bg-zinc-500 text-white fixed w-full top-0 left-0 shadow-md z-50 font-mono">
       <div className="flex justify-between items-center px-4 py-3">
-        <div className="flex items-center gap-1 ">
-          <img src={jslogo} alt="JS Logo" className="w-10 h-10 rounded-full mb-2.5" />
-          <h4 className="font-mono font-bold">JsCompiler</h4>
-        </div>
+        <Link
+          to={"/"}
+          style={{ textDecoration: "none" }}
+          className="flex items-center gap-1 no-underline"
+        >
+          <img
+            src={jslogo}
+            alt="JS Logo"
+            className="w-10 h-10 rounded-full mb-2.5"
+          />
+          <h4 className="font-mono font-bold text-white">JsCompiler</h4>
+        </Link>
 
         <ul className="hidden md:flex items-center gap-6 text-md font-medium">
           <li>
-            <Link style={{textDecoration:"none"}} to="/" className="no-underline text-white" >
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/"
+              className="no-underline text-white"
+            >
               Home
             </Link>
           </li>
           <li>
-            <Link style={{textDecoration:"none"}} to="/playground" className=" text-white">
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/playground"
+              className=" text-white"
+            >
               Playground
             </Link>
           </li>
           <li>
-            <Link style={{textDecoration:"none"}} to="/docs" className=" text-white">
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/docs"
+              className=" text-white"
+            >
               Docs
             </Link>
           </li>
           <li>
-            <Link style={{textDecoration:"none"}} to="/examples" className=" text-white">
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/examples"
+              className=" text-white"
+            >
               Examples
             </Link>
           </li>
         </ul>
 
-        <button className="md:hidden focus:outline-none" onClick={() => setIsOpen(!isOpen)}>
-
-          <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-
-            {isOpen ? 
-
-            (<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />) 
-              : 
-            (<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16"/>)
-            }
-
+        <button
+          className="md:hidden focus:outline-none"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <svg
+            className="w-6 h-6 text-white"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+          >
+            {isOpen ? (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            ) : (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            )}
           </svg>
         </button>
       </div>
@@ -56,17 +94,32 @@ function Header() {
       {isOpen && (
         <ul className="md:hidden flex flex-col items-center px-6 pb-4 gap-3 text-md font-medium">
           <li>
-            <Link style={{textDecoration:"none"}} to="/" onClick={() => setIsOpen(false)} className="no-underline text-white">
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/"
+              onClick={() => setIsOpen(false)}
+              className="no-underline text-white"
+            >
               Home
             </Link>
           </li>
           <li>
-            <Link style={{textDecoration:"none"}} to="/docs" onClick={() => setIsOpen(false)} className="no-underline text-white">
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/docs"
+              onClick={() => setIsOpen(false)}
+              className="no-underline text-white"
+            >
               Docs
             </Link>
           </li>
           <li>
-            <Link style={{textDecoration:"none"}} to="/examples" onClick={() => setIsOpen(false)} className="no-underline text-white">
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/examples"
+              onClick={() => setIsOpen(false)}
+              className="no-underline text-white"
+            >
               Examples
             </Link>
           </li>
@@ -77,5 +130,3 @@ function Header() {
 }
 
 export default Header;
-
-
